b76f260ca9c4d60dd3275a8bec07b2744b22dd88
Uri Gorelik <uri.gore@gmail.com>
2017-04-17 22:06:21 -0400

    Simplified Buffer to use Agent instead of implement GenServer behaviour

--- lib/exstream_twitter/buffer.ex
+++ lib/exstream_twitter/buffer.ex
@@ -1,36 +1,21 @@
 defmodule ExstreamTwitter.Buffer do
-  use GenServer
-
   def start_link do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    Agent.start_link(fn -> [] end, name: __MODULE__)
   end
 
   # Public API
   def add tweet do
-    GenServer.cast(__MODULE__, {:add, tweet})
+    Agent.update(__MODULE__, fn tweets -> [ tweet | tweets ] end)
   end
 
   def get do
-    GenServer.call(__MODULE__, :get)
+    Agent.get(__MODULE__, fn [ h | tweets ] -> h end)
   end
 
   def pop do
-    GenServer.call(__MODULE__, :pop)
+    Agent.get_and_update(__MODULE__, fn
+      [] -> {nil, []}
+      [ tweet | tweets ] -> {tweet, tweets}
+    end)
   end
-
-  # GenServer API
-  def handle_cast({:add, tweet}, state) when length(state) >= 100 do
-    {:noreply, List.delete_at( [tweet | state], -1 )}
-  end
-  def handle_cast({:add, tweet}, state), do: {:noreply, [ tweet | state ]}
-
-  def handle_call(:get, _from, [tweet | t] = state), do: {:reply, tweet, state}
-  def handle_call(:get, _from, [tweet] = state), do: {:reply, tweet, state}
-  def handle_call(:get, _from, []), do: {:reply, nil, []}
-
-  def handle_call(:pop, _from, [tweet | t] = state), do: {:reply, tweet, t}
-  def handle_call(:pop, _from, [tweet] = state), do: {:reply, tweet, []}
-  def handle_call(:pop, _from, []), do: {:reply, nil, []}
-
-  def handle_call(:get_all, _from, state), do: {:reply, state, state}
 end
